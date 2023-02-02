c67f106d0d1db0d4e81d6b92e01b5b3152d6fbef
joost van de Bovenkamp <joost.vd.bovenkamp@outlook.com>
2023-02-02 21:42:23 +0100

    Add migration

--- Monolith.Warehouse.Infra/Migrations/20230202204034_add-count-for-inventory.Designer.cs
+++ Monolith.Warehouse.Infra/Migrations/20230202204034_add-count-for-inventory.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Warehouse.Infra;
@@ -11,9 +12,11 @@ using Warehouse.Infra;
 namespace Warehouse.Infra.Migrations
 {
     [DbContext(typeof(WarehouseDbContext))]
-    partial class WarehouseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230202204034_add-count-for-inventory")]
+    partial class addcountforinventory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- README.md
+++ README.md
@@ -4,6 +4,12 @@
 Purpose of this kata is to extract microservices from the Monolith. The code is still under construction.
 
 
+## scratch notes
+add migration in warehouse infra project:
+
+run the following command from Monolith.API folder
+dotnet ef migrations add add-count-for-inventory  -c WarehouseDbContext -p ..\Monolith.Warehouse.Infra -o Migrations
+
 ## Credits 
 
 Code in the warehouse assembly is a direct copy from Emily Bache refactor Kata Gilded Rose. The original code can be found at the following location: https://github.com/emilybache/GildedRose-Refactoring-Kata
