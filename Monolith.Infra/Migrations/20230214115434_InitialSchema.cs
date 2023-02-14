582de4f105bcab3b8b92e416741e94b244889e36
joost van de Bovenkamp <joost.vd.bovenkamp@outlook.com>
2023-02-14 12:55:02 +0100

    add initial migration

---
+++ Monolith.Infra/Migrations/20230214115434_InitialSchema.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Warehouse.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "shoppingcart");
+
+            migrationBuilder.EnsureSchema(
+                name: "ordering");
+
+            migrationBuilder.EnsureSchema(
+                name: "warehouse");
+
+            migrationBuilder.CreateTable(
+                name: "Cart",
+                schema: "shoppingcart",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerNumber = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cart", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Order",
+                schema: "ordering",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CustomerNumber = table.Column<string>(type: "text", nullable: false),
+                    TotalPrice = table.Column<double>(type: "double precision", nullable: false),
+                    TotalWithTax = table.Column<double>(type: "double precision", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StockItem",
+                schema: "warehouse",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    SellIn = table.Column<int>(type: "integer", nullable: false),
+                    Quality = table.Column<int>(type: "integer", nullable: false),
+                    Count = table.Column<int>(type: "integer", nullable: false),
+                    ReservedCount = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StockItem", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CartItem",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    CartId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Amount = table.Column<int>(type: "integer", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CartItem_Cart_CartId",
+                        column: x => x.CartId,
+                        principalSchema: "shoppingcart",
+                        principalTable: "Cart",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderLine",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Price = table.Column<double>(type: "double precision", nullable: false),
+                    ProductCode = table.Column<string>(type: "text", nullable: false),
+                    TotalOrdered = table.Column<int>(type: "integer", nullable: false),
+                    OrderId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderLine", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderLine_Order_OrderId",
+                        column: x => x.OrderId,
+                        principalSchema: "ordering",
+                        principalTable: "Order",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CartItem_CartId",
+                table: "CartItem",
+                column: "CartId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderLine_OrderId",
+                table: "OrderLine",
+                column: "OrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItem");
+
+            migrationBuilder.DropTable(
+                name: "OrderLine");
+
+            migrationBuilder.DropTable(
+                name: "StockItem",
+                schema: "warehouse");
+
+            migrationBuilder.DropTable(
+                name: "Cart",
+                schema: "shoppingcart");
+
+            migrationBuilder.DropTable(
+                name: "Order",
+                schema: "ordering");
+        }
+    }
+}
--- Monolith.Simulation/PersonasOrDepartments/GoodsReceiving/GoodsReceivedScenario.cs
+++ Monolith.Simulation/PersonasOrDepartments/GoodsReceiving/GoodsReceivedScenario.cs
@@ -5,14 +5,22 @@ namespace Monolith.Simulation.PersonasOrDepartments.GoodsReceiving;
 public class GoodsReceivedScenario
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IWarehouseRepository _warehouseRepository;
 
-    public GoodsReceivedScenario(IUnitOfWork unitOfWork)
+    public GoodsReceivedScenario(IUnitOfWork unitOfWork, IWarehouseRepository warehouseRepository)
     {
         _unitOfWork = unitOfWork;
+        _warehouseRepository = warehouseRepository;
     }
 
     public async Task RunScenario()
     {
+        // get a handle on how much is needed in pending orders..add 50
+        var currentStock = await _warehouseRepository.GetAllAsync();
+
+        var groups = currentStock.GroupBy(item => item.Name);
+        
+        
         await _unitOfWork.SaveChangesAsync();
     }
 }
