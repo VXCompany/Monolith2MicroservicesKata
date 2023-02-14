7660a005263fd6f6eb87a3cef6a1891a351504b3
joost van de Bovenkamp <joost.vd.bovenkamp@outlook.com>
2023-02-14 13:23:45 +0100

    add migrations

---
+++ Monolith.Infra/Migrations/20230214122307_cart-productid-to-productname.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Warehouse.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class cartproductidtoproductname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "CartItem");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProductCode",
+                table: "CartItem",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductCode",
+                table: "CartItem");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ProductId",
+                table: "CartItem",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
