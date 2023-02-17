f6541ad4974cdc66187f1790cb0032974b56ff25
joost van de Bovenkamp <joost.vd.bovenkamp@outlook.com>
2023-02-17 21:05:53 +0100

    add pickorder migration

---
+++ Monolith.Infra/Migrations/20230217112009_Add-pickorder.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Warehouse.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addpickorder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PickOrder",
+                schema: "warehouse",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ForOrder = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PickOrder", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PickOrderLine",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ProductCode = table.Column<string>(type: "text", nullable: false),
+                    Amount = table.Column<int>(type: "integer", nullable: false),
+                    PickOrderId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PickOrderLine", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PickOrderLine_PickOrder_PickOrderId",
+                        column: x => x.PickOrderId,
+                        principalSchema: "warehouse",
+                        principalTable: "PickOrder",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PickOrderLine_PickOrderId",
+                table: "PickOrderLine",
+                column: "PickOrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PickOrderLine");
+
+            migrationBuilder.DropTable(
+                name: "PickOrder",
+                schema: "warehouse");
+        }
+    }
+}
