33257228b54871f84793aca0667e9b99c5267728
joost van de Bovenkamp <joost.vd.bovenkamp@outlook.com>
2023-02-17 21:58:14 +0100

    Web Gateway added

---
+++ src/Services/ShoppingCart/ShoppingCart.API/Endpoints/ShoppingCart.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingCart.API.Endpoints;
+
+public static class ShoppingCart
+{
+    public static void ConfigureShoppingCartEndpoints(this WebApplication application)
+    {
+        var basketGroup = application.MapGroup("Basket");
+        
+        // TODO: Add necesarry endpoints
+        /*basketGroup.MapGet("/{customerNumber}", GetBasket);
+        basketGroup.MapPost("/{customerNumber}", AddProduct);
+
+        basketGroup.MapPost("/{customerId}/checkout", CheckoutBasket);
+        */
+    }
+}
