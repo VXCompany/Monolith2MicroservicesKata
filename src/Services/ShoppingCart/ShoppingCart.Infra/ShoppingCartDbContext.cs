7f7381fb04a37950e2ec0ee87a28bea59c311019
joost van de Bovenkamp <joost.vd.bovenkamp@outlook.com>
2023-02-21 16:41:07 +0100

    Add shopping cart db context

---
+++ src/Services/ShoppingCart/ShoppingCart.Infra/ShoppingCartDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Infra.Data;
+
+namespace ShoppingCart.Infra;
+
+public class ShoppingCartDbContext : DbContext, IUnitOfWork
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder
+            .Entity<Cart>()
+            .ToTable(nameof(Cart), "shoppingcart")
+            .HasKey(p => p.Id);
+        modelBuilder
+            .Entity<Cart>()
+            .HasMany(c => c.Items)
+            .WithOne(ci => ci.Cart);
+    }
+
+    public Task<int> SaveChangesAsync()
+    {
+        return base.SaveChangesAsync();
+    }
+}
